d086a51b9c949a001aee786c299df17f2289c8f1
jstnde <nachnitsuj@gmail.com>
2021-11-19 16:49:54 +0800

    Updated "animated" word

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 setInterval(function() {
     const words = document.getElementsByClassName("word");
     let currentId = 1;
-    let nextId;
 
     for (const word of words) {
         if (word.classList.contains("active")) {
@@ -9,12 +8,7 @@ setInterval(function() {
         }
     }
 
-    if (currentId === words.length) {
-        nextId = 1;
-    }
-    else {
-        nextId = currentId + 1;
-    }
+    const nextId = currentId === words.length ? 1 : currentId + 1
 
     for (const word of words) {
         word.classList.remove("active");
