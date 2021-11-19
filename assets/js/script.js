e0b733a5f48e5372693149f2d54c68d15bfdcdcc
jstnde <nachnitsuj@gmail.com>
2021-11-19 22:50:58 +0800

    Prev and next button for about-me page

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ setInterval(function() {
         }
     }
 
-    const nextId = currentId === words.length ? 1 : currentId + 1
+    const nextId = currentId === words.length ? 1 : currentId + 1;
 
     for (const word of words) {
         word.classList.remove("active");
@@ -17,3 +17,34 @@ setInterval(function() {
         }
     }
 }, 3000);
+
+const arrowBtn = document.getElementsByClassName("arrow")
+
+for (const btn of arrowBtn) {
+    btn.addEventListener("click", function() {
+        const about = document.getElementsByClassName("about")[0];
+        const aboutPages = about.children;
+        let currentId = 1;
+        let nextId;
+
+        for (const aboutPage of aboutPages) {
+            if (aboutPage.classList.contains("active")) {
+                currentId = parseInt(aboutPage.attributes.getNamedItem("data-id").value);
+            }
+        }
+
+        if (this.classList.contains("prev")) {
+            nextId = currentId === 1 ? aboutPages.length : currentId - 1;
+        }
+        else {
+            nextId = currentId === aboutPages.length ? 1 : currentId + 1;
+        }
+
+        for (const aboutPage of aboutPages) {
+            aboutPage.classList.remove("active");
+            if (parseInt(aboutPage.attributes.getNamedItem("data-id").value) === nextId) {
+                aboutPage.classList.add("active");
+            }
+        }
+    });
+}
